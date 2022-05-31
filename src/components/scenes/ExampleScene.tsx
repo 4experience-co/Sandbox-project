1f3921687185abff9ed80d5b3175450bb91a928a
Karol Szcześniak <karolszczesniakk@gmail.com>
2022-05-31 16:01:32 +0200

    [RND-89] scipts

--- src/components/scenes/ExampleScene.tsx
+++ src/components/scenes/ExampleScene.tsx
@@ -1,42 +1,19 @@
 import 'aframe';
+
 import { Sphere, Cylinder, Plane, Sky } from '@belivvr/aframe-react';
-import { Entity } from 'aframe-react';
+
 
 function ExampleScene() {
   return (
-    <a-scene embedded>
-      <Entity
-        super-hands
-        oculus-touch-controls={{ hand: 'right' }}
-        events={{
-          bbuttondown: () => console.log('b clicked'),
-          abuttondown: () => console.log('a clicked'),
-          triggerdown: () => console.log('triggerdown'),
-          thumbsticktouchstart: () => console.log('thumbsticktouchstart'),
-          gripdown: () => console.log('a gripdown'),
-        }}
-      />
-      <a-box position=' -1 0.5 -3' rotation='0 45 0' color='#87a128' />
-      <Sphere
-        position={{ x: 0, y: 1.25, z: -5 }}
-        radius={1.25}
-        color='#EF2D5E'
-      />
-      <Cylinder
-        position={{ x: 1, y: 0.75, z: -3 }}
-        radius={0.5}
-        height={1.5}
-        color='#FFC65D'
-      />
-      <Plane
-        position={{ x: 0, y: 0, z: -4 }}
-        rotation={{ x: -90, y: 0, z: 0 }}
-        width={4}
-        height={4}
-        color='#7BC8A4'
-      />
-      <Sky color='#ECECEC' />
-    </a-scene>
+      <a-scene embedded>
+    <a-entity>
+      <a-camera></a-camera>
+      <a-entity sphere-collider="objects: a-box" super-hands hand-controls="hand: left"></a-entity>
+      <a-entity sphere-collider="objects: a-box" super-hands hand-controls="hand: right"></a-entity>
+    </a-entity>
+  
+    <a-box hoverable grabbable stretchable draggable dropppable color="blue" position="1 0 0"></a-box>
+  </a-scene>
   );
 }
 
